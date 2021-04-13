2d21969235b49f4bca48995497b1583df26d30c1
subilphilip <56921890+subilphilip@users.noreply.github.com>
2021-04-13 21:35:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from kivy_garden.mapview import MapView
 from kivy.app import App
-from kivy.uix.button import  Button
-class LangLearnApp(App):
-    def build(self ):
-        return Button(text = "hello dumbass")
 
-if __name__== "__main__":
-    LangLearnApp().run()
+class MapViewApp(App):
+    def build(self):
+        mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
+        return mapview
+
+MapViewApp().run()
